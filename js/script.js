3297d4dbc80a30659243dc494b600626bf1eec83
Burgeonz <dimasaqilsabil@gmail.com>
2024-01-18 01:09:17 +0700

    last commit

--- js/script.js
+++ js/script.js
@@ -3,13 +3,23 @@ function calculateSquare() {
 
 
 if (isNaN(side) || side <= 0) {
-    document.getElementById("output").innerHTML = "Masukkan angka positif yang valid untuk panjang sisi.";
+    document.getElementById("output").innerHTML = "Masukkan angka positif yang valid agar dapat dihitung.";
     return;
 }
 
 var area = side * side;
 var perimeter = 4 * side;
 
-document.getElementById("output").innerHTML = "Luas Persegi: " + area.toFixed();
+document.getElementById("output").innerHTML =
+"Panjang Sisi: " + side.toFixed() +
+"<br>Luas Persegi: " + area.toFixed() +
+"<br>Rumus Luas: " + side.toFixed() + " * " + side.toFixed() + " = " + (side * side).toFixed() +
+"<br>Keliling Persegi: " + perimeter.toFixed() +
+"<br>Rumus Keliling: 4 * " + side.toFixed() + " = " + perimeter.toFixed();
 }
 
+function resetCalculator() {
+   
+    document.getElementById("textbox").value = "";
+    document.getElementById("output").innerHTML = "";
+}
